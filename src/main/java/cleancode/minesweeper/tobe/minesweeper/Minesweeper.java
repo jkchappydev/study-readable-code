d00804b3870e109887b222003101ec24b2a5dbc2
jkchappydev <jkchappydev@gmail.com>
2025-02-17 23:52:15 +0900

    study: section6 - 패키지 나누기

---
+++ src/main/java/cleancode/minesweeper/tobe/minesweeper/Minesweeper.java
@@ -0,0 +1,131 @@
+package cleancode.minesweeper.tobe.minesweeper;
+
+import cleancode.minesweeper.tobe.minesweeper.board.GameBoard;
+import cleancode.minesweeper.tobe.minesweeper.exception.GameException;
+import cleancode.minesweeper.tobe.game.GameInitializable;
+import cleancode.minesweeper.tobe.game.GameRunnable;
+import cleancode.minesweeper.tobe.minesweeper.io.InputHandler;
+import cleancode.minesweeper.tobe.minesweeper.io.OutputHandler;
+import cleancode.minesweeper.tobe.minesweeper.io.config.GameConfig;
+import cleancode.minesweeper.tobe.minesweeper.board.position.CellPosition;
+import cleancode.minesweeper.tobe.minesweeper.user.UserAction;
+
+public class Minesweeper implements GameInitializable, GameRunnable {
+
+    private final GameBoard gameBoard; // Board 입장에서는 GameBoard 로 객체화 및 내부에서 캡슐화 되어있어서, 데이터 구조에 대해서는 모른다.
+    // DIP
+    // Minesweeper 입장 에서는 얘네들은 너무 저수준 모듈임
+    // 사용자는 현재 console로만 이 게임과 소통을 할 수 있음
+    // 만약, 이 지뢰찾기가 어떤 웹페이지에서 동작하는 거라면?
+    // Minesweeper이라는 지뢰찾기의 도메인 룰(=규칙)은 변하지 않지만, console 이냐 web 이냐가 변경됨.
+    // 이 지뢰찾기를 웹 페이지에 올리기 위해서는 현재 consoleInputHandler과 consoleOutputHandler가 직접적으로 박혀 있기 때문에,
+    // 해당 두 개의 기능에 변경이 불가피하다. 하지만 사용자와 소통한다는 기능 자체는 변함이 없음.
+    // private final ConsoleInputHandler inputHandler = new ConsoleInputHandler();
+    // private final ConsoleOutputHandler outputHandler = new ConsoleOutputHandler();
+    private final InputHandler inputHandler;
+    private final OutputHandler outputHandler;
+
+    // DIP : InputHandler와 OutputHandler을 외부에서 받도록 구현
+    // Minesweeper은 InputHandler와 OutputHandler라는 인터페이스만 알고 있음. 인터페이스만 받아서 사용하고, 실제로 어떤게 들어오는지는 신경쓰지 않아도 됨
+    // 고수준 모듈(Minesweeper)이 InputHandler, OutputHandler의 추상화에만 의존하게 됨.
+    // 기존에는 저수준 모듈인 ConsoleInputHandler, ConsoleOutputHandler 직접 사용
+//    public Minesweeper(GameLevel gameLevel, InputHandler inputHandler, OutputHandler outputHandler) {
+//        gameBoard = new GameBoard(gameLevel); // gameBoard를 생성할 때, gameLevel을 전달함
+//        this.inputHandler = inputHandler;
+//        this.outputHandler = outputHandler;
+//    }
+
+    public Minesweeper(GameConfig gameConfig) {
+        gameBoard = new GameBoard(gameConfig.getGameLevel());
+        this.inputHandler = gameConfig.getInputHandler();
+        this.outputHandler = gameConfig.getOutputHandler();
+    }
+
+    @Override
+    public void initialize() {
+        gameBoard.initializeGame();
+    }
+
+    @Override
+    public void run() {
+        outputHandler.showGameStartComments();
+
+        while (gameBoard.isInProgress()) {
+            try {
+                outputHandler.showBoard(gameBoard);
+
+                CellPosition cellPosition = getCellInputFromUser();
+                UserAction userActionInput = getUserActionInputFromUser();
+                actOnCell(cellPosition, userActionInput);
+            } catch (GameException e) {
+                // 개발자가 의도한 예외 상황 (예외 상황에 대한 메세지 출력)
+                outputHandler.showExceptionMessage(e);
+            } catch (Exception e) {
+                // 프로그램에서 처리한 예외 상황 (단순 메세지 출력)
+                outputHandler.showSimpleMessage("프로그램에 문제가 생겼습니다.");
+                // e.printStackTrace(); // 실무에서는 안티 패턴. log 사용
+            }
+        }
+
+        outputHandler.showBoard(gameBoard);
+
+        if (gameBoard.isWinStatus()) {
+            outputHandler.showGameWinningComment();
+        }
+        if (gameBoard.isLoseStatus()) {
+            outputHandler.showGameLosingComment();
+        }
+    }
+
+    private CellPosition getCellInputFromUser() {
+        outputHandler.showCommentForSelectionCell();
+
+        // CellPosition이라는 Value Object가 만들어 질 때부터 유효성 검사하고, 만들어 지는 순간 인덱스의 기능을 함
+        CellPosition cellPosition = inputHandler.getCellPositionFromUser();
+        if (gameBoard.isInvalidCellPosition(cellPosition)) { // 올바른 cellPosition으로 gameBoard에서 동작할 수 있는가?
+            throw new GameException("잘못된 좌표를 선택하셨습니다.");
+        }
+
+        return cellPosition;
+    }
+
+    private UserAction getUserActionInputFromUser() {
+        outputHandler.showCommentForUserAction();
+        return inputHandler.getUserActionFromUser();
+    }
+
+    private void actOnCell(CellPosition cellPosition, UserAction userActionInput) {
+        if (doesUserChooseToPlantFlag(userActionInput)) {
+            gameBoard.flagAt(cellPosition);
+            return;
+        }
+
+        if (doesUserChooseToOpenCell(userActionInput)) {
+            gameBoard.openAt(cellPosition);
+            return;
+        }
+
+        throw new GameException("잘못된 번호를 선택하셨습니다.");
+    }
+
+    private boolean doesUserChooseToOpenCell(UserAction userAction) {
+        return userAction == UserAction.OPEN;
+    }
+
+    private boolean doesUserChooseToPlantFlag(UserAction userAction) {
+        return userAction == UserAction.FLAG;
+    }
+
+    // 만약에, 여러군데에서 사용하는 메서드라면, 해당 메서드를 수정 또는 변경 했을 때 해당 메서드를 사용하는 곳이 모두 영향이 간다.
+//    private static boolean isAllCellOpened() {
+//        boolean isAllOpened = true;
+//        for (int row = 0; row < BOARD_ROW_SIZE; row++) {
+//            for (int col = 0; col < BOARD_COL_SIZE; col++) {
+//                if (BOARD[row][col].equals(CLOSED_CELL_SIGN)) {
+//                    isAllOpened = false;
+//                }
+//            }
+//        }
+//        return isAllOpened;
+//    }
+}
---
+++ src/main/java/cleancode/minesweeper/tobe/minesweeper/io/ConsoleOutputHandler.java
@@ -0,0 +1,87 @@
+package cleancode.minesweeper.tobe.minesweeper.io;
+
+import cleancode.minesweeper.tobe.minesweeper.board.GameBoard;
+import cleancode.minesweeper.tobe.minesweeper.exception.GameException;
+import cleancode.minesweeper.tobe.minesweeper.board.cell.CellSnapshot;
+import cleancode.minesweeper.tobe.minesweeper.board.position.CellPosition;
+import cleancode.minesweeper.tobe.minesweeper.io.sign.CellSignProvider;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class ConsoleOutputHandler implements OutputHandler {
+
+    // private final CellSignFinder cellSignFinder = new CellSignFinder();
+
+    @Override
+    public void showGameStartComments() {
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        System.out.println("지뢰찾기 게임 시작!");
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+    }
+
+    @Override
+    public void showBoard(GameBoard board) {
+        String alphabets = generateColAlphabets(board);
+        System.out.println("    " + alphabets);
+        
+        // BOARD_ROW_SIZE, BOARD_COL_SIZE는 BOARD로 부터 가져올 수 있다. 따라서 인자를 BOARD를 받음
+        for (int row = 0; row < board.getRowSize(); row++) { // board 안쪽으로 cell이 숨어들어갔기 때문에 board한테 rowSize좀 줄래? 해서 getRowSize()
+            System.out.printf("%2d  ", row + 1);
+            for (int col = 0; col < board.getColSize(); col++) {
+                CellPosition cellPosition = CellPosition.of(row, col);
+
+                CellSnapshot snapshot = board.getSnapshot(cellPosition);
+                // String cellSign = cellSignFinder.findCellSignFrom(snapshot);
+                String cellSign = CellSignProvider.findCellSignFrom(snapshot);
+
+                System.out.print(cellSign + " "); // getter를 사용하는 이유 : board를 그리는 쪽은 여긴데, cell에다가 board를 그려줘 하는 것은 관심사가 쪼개짐
+            } //board.getCell().getSign() <- 무례한 것 (혹시 row, col 인덱스에 있는 cell의 sign좀 줄래? 라고 변경)
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    private String generateColAlphabets(GameBoard board) {
+        // range: index=0부터 시작해서 board의 getColSize까지 range를 만듬
+        // mapToObj: 어떤 0부터 시작하는 index가 만들어졌는데, 'a'라는 문자열에 index를 더하겠디. index는 0부터 시작 ('a' + 0 = 'a', 'a' + 1 = 'b', ..)
+        // map: char를 toString()으로 변환
+        // toList: List로 변환
+        List<String> alphabets = IntStream.range(0, board.getColSize())
+                        .mapToObj(index -> (char) ('a' + index))
+                                .map(Object::toString)
+                                        .toList();
+        return String.join(" ", alphabets);
+    }
+
+    @Override
+    public void showGameWinningComment() {
+        System.out.println("지뢰를 모두 찾았습니다. GAME CLEAR!");
+    }
+
+    @Override
+    public void showGameLosingComment() {
+        System.out.println("지뢰를 밟았습니다. GAME OVER!");
+    }
+
+    @Override
+    public void showCommentForSelectionCell() {
+        System.out.println("선택할 좌표를 입력하세요. (예: a1)");
+    }
+
+    @Override
+    public void showCommentForUserAction() {
+        System.out.println("선택한 셀에 대한 행위를 선택하세요. (1: 오픈, 2: 깃발 꽂기)");
+    }
+
+    @Override
+    public void showExceptionMessage(GameException e) {
+        System.out.println(e.getMessage()); // 개발자가 의도한 예외
+    }
+
+    @Override
+    public void showSimpleMessage(String message) {
+        System.out.println(message);
+    }
+
+}
